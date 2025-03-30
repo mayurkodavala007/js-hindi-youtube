ba65f37ef58f8efd5efe296b30271ef3a7f96105
mayurkodavala007 <mayurkodavala18@gmail.com>
2025-03-30 16:59:10 +0000

    container config done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as never version
+
+console.log(3+3)
+console.log("mayur")
+
+let name = "mayur"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to [power 53]
+//bigint
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+console.log(typeof undefined);
