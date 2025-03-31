94245029bad5bc6e62b422229ffacd15aad5d8e8
mayurkodavala007 <mayurkodavala18@gmail.com>
2025-03-31 15:38:51 +0000

    conversion config datatype

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -23,6 +23,44 @@ let BooleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ************  oprations  ****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/3);
+//console.log(2**3);
+//console.log(2%3);
+
+let str1 = "mayur"
+let str2 = "hello"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+    
+
+
+
 
